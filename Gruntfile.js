d5ba1da82d4dd127915aa09d9b2d6631d5f34db5
blacroix <benjlacroix@gmail.com>
2014-09-10 20:45:31 +0200

    Refactor packaging

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,16 +8,39 @@ module.exports = function (grunt) {
         clean: {
             dist: ['.tmp', 'dist/*']
         },
-        cssmin: {
-            dist: {
-                files: {
-                    'dist/css/main.css %>': [
-                        'css/*.css'
-                    ]
-                }
-            }
-        },
         copy: {
+            index: {
+                options: {
+                    process: function (content) {
+                        content = content.replace(/<!--JS0-->[\s\S]*<!--JS0-->/, '<script src="app.min.js?v=' + new Date().getTime() + '"></script>');
+                        return content;
+                    }
+                },
+                src: 'index.html',
+                dest: 'dist/index.html'
+            },
+            worker: {
+                options: {
+                    process: function (content) {
+                        content = content.replace(/<!--JS0-->[\s\S]*<!--JS0-->/, '<script src="app.min.js?v=' + new Date().getTime() + '"></script>');
+                        content = content.replace(/<!--JS1-->[\s\S]*<!--JS1-->/, '<script src="worker.min.js?v=' + new Date().getTime() + '"></script>');
+                        return content;
+                    }
+                },
+                src: 'worker.html',
+                dest: 'dist/worker.html'
+            },
+            user: {
+                options: {
+                    process: function (content) {
+                        content = content.replace(/<!--JS0-->[\s\S]*<!--JS0-->/, '<script src="app.min.js?v=' + new Date().getTime() + '"></script>');
+                        content = content.replace(/<!--JS1-->[\s\S]*<!--JS1-->/, '<script src="user.min.js?v=' + new Date().getTime() + '"></script>');
+                        return content;
+                    }
+                },
+                src: 'user.html',
+                dest: 'dist/user.html'
+            },
             dist: {
                 files: [
                     {
@@ -28,16 +51,43 @@ module.exports = function (grunt) {
                         src: [
                             'favicon.png',
                             'fonts/*',
-                            'css/ie/*'
+                            'css/**'
                         ]
                     }
                 ]
             }
         },
-        // TODO CSS (minify skell and style, minify others)
-        // TODO JS (minify and concat)
-        // TODO COPY + REPLACE (index, worker and user)
-        // TODO MINIFY HTML
+        uglify: {
+            dist: {
+                files: {
+                    'build/app.min.js': ['js/init.js'],
+                    'dist/user.min.js': ['js/user.js'],
+                    'dist/worker.min.js': ['js/worker.js']
+                }
+            }
+        },
+        imagemin: {
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'images/',
+                        src: ['**/*.{png,jpg}'],
+                        dest: 'dist/images/'
+                    }
+                ]
+            }
+        },
+        concat: {
+            options: {
+                separator: ';',
+                stripBanners: true
+            },
+            dist: {
+                src: ['js/jquery.min.js', 'js/jquery.dropotron.min.js', 'js/skel.min.js', 'js/skel-layers.min.js', 'build/app.min.js'],
+                dest: 'dist/app.min.js'
+            }
+        },
         'gh-pages': {
             options: {
                 base: 'dist'
@@ -48,6 +98,12 @@ module.exports = function (grunt) {
 
     grunt.registerTask('package', [
         'clean:dist',
+        'uglify:dist',
+        'concat:dist',
+        'imagemin:dist',
+        'copy:index',
+        'copy:worker',
+        'copy:user',
         'copy:dist'
     ]);
 
